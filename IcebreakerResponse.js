3b9232e06c22ad17f23f56b3ab8ad49e18f622d7
Alice Freda <alicefreda@gmail.com>
2017-11-16 22:48:32 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,7 +2,7 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
-IcebreakerResponse.BatchCreate = function(data){
+IcebreakerResponse.BatchCreate = function(emails){
   return data.map(function(userData){
     return new IcebreakerResponse(userData[0], userData[1])
   })
@@ -13,4 +13,4 @@ IcebreakerResponse.BatchCreate = function(data){
 //   ["Alan", "alan@xparc.com"]
 // ]
 
-let responses = IcebreakerResponse.BatchCreate(data);
+let icebreakerResponses = IcebreakerResponse.BatchCreate(emails);
