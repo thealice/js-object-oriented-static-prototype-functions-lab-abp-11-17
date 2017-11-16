0aad9ef6ce075ffc3e12687975746055c21afe61
Alice Freda <alicefreda@gmail.com>
2017-11-16 22:56:59 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,11 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails){
+  return emails.map(function(icebreakerData){
+    return new IcebreakerResponse(icebreakerData);
+  })
+}
+// const emails = ["avi@flatironschool.com", "grace@hopper.com", "alan@xparc.com"];
+//
+// let icebreakerResponses = IcebreakerResponse.BatchCreate(emails);
