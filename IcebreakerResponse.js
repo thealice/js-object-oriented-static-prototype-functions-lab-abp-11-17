b39746243fc723a6ca439ce19b0896a50c856e14
Alice Freda <alicefreda@gmail.com>
2017-11-16 22:46:35 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -7,10 +7,10 @@ IcebreakerResponse.BatchCreate = function(data){
     return new IcebreakerResponse(userData[0], userData[1])
   })
 }
-const data = [
-  ["Avi", "avi@flatironschool.com"],
-  ["Grace", "grace@hopper.com"],
-  ["Alan", "alan@xparc.com"]
-]
- 
+// const data = [
+//   ["Avi", "avi@flatironschool.com"],
+//   ["Grace", "grace@hopper.com"],
+//   ["Alan", "alan@xparc.com"]
+// ]
+
 let responses = IcebreakerResponse.BatchCreate(data);
